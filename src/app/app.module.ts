d3ebaa10ca295cdd7af6b13e4496080a24cccc26
Mathias <mathiaslucero19@gmail.com>
2021-11-29 10:12:15 -0300

    geolocalizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    NativeGeocoder,
+    Geolocation, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderOptions, NativeGeocoderResult } from '@ionic-native/native-geocoder/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  lat: any; 
+  long: any;
+  address: any;
+  accuracy: any;
+
+  constructor( private geolocation: Geolocation, private nativeGeocoder: NativeGeocoder) {}
+  
+  ngOnInit() {
+    this.geoInformation();
+  }
+
+  options = {
+    timeout: 12000, 
+    maximumAge: 3600,
+    enableHighAccuracy: true, 
+  };
+
+  geoInformation() {
+    this.geolocation.getCurrentPosition().then((data) => {
+      this.lat = data.coords.latitude;
+      this.long = data.coords.longitude;
+      this.accuracy = data.coords.accuracy;
+
+      this.cordsToAddress(this.lat, this.long);
+     }).catch((error) => {
+       alert(error);
+     });
+  }
+
+  // geocoder options
+  nativeGeocoderOptions: NativeGeocoderOptions = {
+    maxResults: 6,
+    useLocale: true
+  };
+
+  // reverse geocode
+  cordsToAddress(lat, long){
+    this.nativeGeocoder.reverseGeocode(lat, long, this.nativeGeocoderOptions)
+    .then((response: NativeGeocoderResult[]) => {
+      this.address = this.createAddress(response[0]);
+    }).catch((error: any) => {
+      alert(JSON.stringify(error));
+    });
+  }
+
+  // Create address
+  createAddress(addressObject){
+    let object = [];
+    let address = "";
+    for (let key in addressObject) {
+      object.push(addressObject[key]);
+    }
+    object.reverse();
+    for (let val in object) {
+      if(object[val].length)
+      address += object[val]+', ';
+    }
+    return address.slice(0, -2);
+  }
 
 }
